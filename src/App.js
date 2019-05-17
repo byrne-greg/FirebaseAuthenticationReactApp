3fd7e988e9c73d046bbad7391170444ee0029a7f
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2019-05-17 15:16:30 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Navigation from "./components/Navigation";
+
+const App = () => (
+  <>
+    <h1>Work in Progress</h1>
+    <Router>
+      <Navigation />
+    </Router>
+  </>
+);
+
+export default App;
