79afe5ed1b05a86bae355a4f6388c29d58c81c8d
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2019-06-05 22:22:22 +0100

    updating testutil

--- src/TestUtil.js
+++ src/TestUtil.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { MemoryRouter } from "react-router-dom";
+import { FirebaseContext } from "./components/Firebase";
+
+const withMockFirebase = (mockFirebase, Component) => (
+  <FirebaseContext.Provider value={mockFirebase}>
+    {Component}
+  </FirebaseContext.Provider>
+);
 
 const withTestRouter = Component => <MemoryRouter>{Component}</MemoryRouter>;
 
-export { withTestRouter };
+export { withTestRouter, withMockFirebase };
