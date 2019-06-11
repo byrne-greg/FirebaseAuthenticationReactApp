fabbc59a6a481e57d8195913fd994e061fe2b436
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2019-06-11 15:31:09 +0100

    remove commented code from setupTests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,10 +1,3 @@
-// import { configure } from "enzyme";
-// import Adapter from "enzyme-adapter-react-16";
-// import { createSerializer } from "enzyme-to-json";
-
-// Configure Enzyme
-// configure({ adapter: new Adapter() });
-
 // Ensure any error/warning/log output to console is flagged as a failing test
 global.console.error = error => {
   throw new Error(error);
@@ -15,6 +8,3 @@ global.console.warn = error => {
 global.console.log = error => {
   throw new Error(error);
 };
-
-// Setup Snapshot Serializer
-// expect.addSnapshotSerializer(createSerializer({ noKey: true, mode: "deep" }));
